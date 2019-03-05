470724f58b449a34f1933492c3b7acb8d01a49db
Haozhe <hzthegreat@gmail.com>
2019-03-05 20:59:48 +0800

    Add functions to transform postfix expression

--- script.sh
+++ script.sh
@@ -1,1 +1,2 @@
-python index.py -i /Users/Haozhe_Haotian/Desktop/y3s2/cs3245/homework/HW2/reuters/training -d dictionary.txt -p postings.txt
+python index.py -i reuters/training -d dictionary.txt -p postings.txt
+python search.py -d dictionary.txt  -p postings.txt -q query.txt -o output.txt
--- search.py
+++ search.py
@@ -3,6 +3,30 @@ import re
 import nltk
 import sys
 import getopt
+import pickle
+
+def deserialize_dictionary(dictionary_file_path):
+    with open(dictionary_file_path) as f:
+        dictionary = pickle.load(f)
+    return dictionary
+
+def get_posting_for_term(term, dictionary, postings_file_path):
+    (offset, length) = dictionary[term]
+    
+    with open(postings_file_path, 'r') as f:
+        f.seek(offset)
+        posting_byte = f.read(length)
+        posting_list = pickle.loads(posting_byte)
+    return posting_list
+
+def transform_postfix(postfix_expression):
+    dictionary = deserialize_dictionary(dictionary_file)
+    operators = {'AND', 'OR', 'NOT'}
+    for i in range(len(postfix_expression)):
+        if postfix_expression[i] not in operators:
+            postfix_expression[i] = \
+                get_posting_for_term(postfix_expression[i], dictionary, postings_file)
+    return postfix_expression
 
 def usage():
     print "usage: " + sys.argv[0] + " -d dictionary-file -p postings-file -q file-of-queries -o output-file-of-results"
@@ -30,3 +54,5 @@ for o, a in opts:
 if dictionary_file == None or postings_file == None or file_of_queries == None or file_of_output == None :
     usage()
     sys.exit(2)
+
+print(transform_postfix(['price', 'level', 'OR']))
