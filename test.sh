4f2370ddf677aefe7eba2d140f5f51cf566112ec
Suyash Shekhar <suyashshekhar@gmail.com>
2019-03-06 13:53:13 +0800

    Update scripts

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # If --o, use the entire reuters data
-if [[ $* == *--o* ]]
+if [[ $* == *-o* ]]
 then
   directory_of_documents=reuters_original/training
 else
@@ -15,4 +15,13 @@ expected_output_file=tests/expected_output1.txt
 
 python index.py -i $directory_of_documents -d $dictionary_file -p $postings_file
 python search.py -d $dictionary_file -p $postings_file -q $file_of_queries -o $output_of_results_file
-diff $output_of_results_file $expected_output_file
+
+cmp --silent $output_of_results_file $expected_output_file && echo '### Test Passed: Files Are Identical! ###' || echo '### Failed: Files Are Different! ###'
+echo "Lines which don't match are:"
+diff $output_of_results_file $expected_output_file | grep '^[1-9]'
+if [[ $* == *-d* ]]
+then 
+  diff $output_of_results_file $expected_output_file
+else
+  echo "Use -d to see the diff between the two files"
+fi
